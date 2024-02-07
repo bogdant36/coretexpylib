8d99356047502a5502db802e8638bd8d84897f33
Bogdan Tintor <btintor@biomech.us>
2024-02-07 09:08:16 +0100

    CTX-5430: Class implementation saving changes...

--- coretex/cli/commands/login.py
+++ coretex/cli/commands/login.py
@@ -1,5 +1,6 @@
 import click
 
+from ..config import CLIConfig
 from ..modules.user import authenticate, saveLoginData
 from ..modules.ui import clickPrompt, stdEcho, successEcho
 from ...configuration import loadConfig, saveConfig, isUserConfigured
@@ -7,10 +8,10 @@ from ...configuration import loadConfig, saveConfig, isUserConfigured
 
 @click.command()
 def login() -> None:
-    config = loadConfig()
-    if isUserConfigured(config):
+    config = CLIConfig()
+    if config.verifyUser():
         if not clickPrompt(
-            f"User already logged in with username {config['username']}.\nWould you like to log in with a different user (Y/n)?",
+            f"User already logged in with username {config.username}.\nWould you like to log in with a different user (Y/n)?",
             type = bool,
             default = True,
             show_default = False
@@ -19,8 +20,8 @@ def login() -> None:
 
     stdEcho("Please enter your credentials:")
     loginInfo = authenticate()
-    config = saveLoginData(loginInfo, config)
+    config.saveLoginData(loginInfo)
 
-    saveConfig(config)
+    config.save()
 
-    successEcho(f"User {config['username']} successfully logged in.")
+    successEcho(f"User {loginInfo.username} successfully logged in.")
